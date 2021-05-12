6f5a9a46818894ed57ab31f55001f06188de4a4d
TheFebrin <dawid.dieu@nomagic.ai>
2021-05-12 12:37:50 +0200

    Model architecture and demo with feeding images

--- utils/download_data_from_bucket.py
+++ utils/download_data_from_bucket.py
@@ -1,48 +1,46 @@
 """
-TODO
+Script to download the data from gcs.
+
+This will download images with ids: 0, 1, 2, ..., 9
+python utils/download_data_from_bucket.py 0 10 color
 """
 
 import sys
 import subprocess
 import multiprocessing
-import concurrent
-import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import Pool
 
-def download_image(id: int, type: str):
+def download_image(id: int, image_type: str) -> None:
 
-    BASE_PATH = f'gs://staging-other-matrix-source-data-nomagic-ai/SBX_COCO_NoMagic_V3_2021_01_26/{type}/Top_NoMagic_'
-    BASE_TARGET = f'{type}/Top_NoMagic_'
+    BASE_PATH = f'gs://staging-other-matrix-source-data-nomagic-ai/SBX_COCO_NoMagic_V3_2021_01_26/{image_type}/Top_NoMagic_'
+    BASE_TARGET = f'{image_type}/Top_NoMagic_'
 
-    def download(id: int):
-        path   = BASE_PATH   + (8 - len(str(id))) * '0' + f'{id}.png'
-        target = BASE_TARGET + (8 - len(str(id))) * '0' + f'{id}.png'
+    path   = BASE_PATH   + (8 - len(str(id))) * '0' + f'{id}.png'
+    target = BASE_TARGET + (8 - len(str(id))) * '0' + f'{id}.png'
 
-        bashCommand = 'gsutil cp ' + path + ' ' + target
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-
-    # TODO: USE THREADS
-    # with concurrent.futures.ThreadPoolExecutor() as executor:
-    #     executor.submit(download, id)
-
-    download(id=id)
-
-    print(f'{id} downloaded.')
+    bashCommand = 'gsutil cp ' + path + ' ' + target
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()  
 
 
 if __name__ == '__main__':
-    type = 'color'
-    n_pictures = 10
+    image_type = 'color'
+    id_image_from, id_image_to = 0, 10
+
     if len(sys.argv) > 1:
-        n_pictures = int(sys.argv[1])
+        id_image_from = int(sys.argv[1])
 
     if len(sys.argv) > 2:
-        type = sys.argv[2]
-        assert type in ['color', 'depth'], f'Wrong type: {type}'
+        id_image_to = int(sys.argv[2])
 
-    for id in range(n_pictures):
-        try:
-            download_image(id=id, type=type)
-        except:
-            print(f'Error while downloading: {id}')
+    if len(sys.argv) > 3:
+        image_type = sys.argv[3]
+        assert image_type in ['color', 'depth'], f'Wrong image_type: {image_type}'
+
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        for id in range(id_image_from, id_image_to):
+            try:
+                executor.submit(download_image, id, image_type)
+            except:
+                print(f'Error while downloading: {id}')
