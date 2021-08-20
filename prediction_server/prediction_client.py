36bd056b14f20c7fdec07cc84450a70f3232e020
TheFebrin <dawid.dieu@nomagic.ai>
2021-08-20 19:37:42 +0200

    Add prediction server

---
+++ prediction_server/prediction_client.py
@@ -0,0 +1,46 @@
+import grpc
+
+import prediction_server_pb2
+import prediction_server_pb2_grpc
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def ping():
+    with grpc.insecure_channel('localhost:50042') as channel:
+        stub = prediction_server_pb2_grpc.PredictionServerStub(channel)
+        response = stub.ping(prediction_server_pb2.PingRequest(message='test ping'))
+    print(f"[Prediction client] [ping] Message from server: {response.message}")
+
+
+def predict():
+    img = np.array(Image.open('../images/color_production_1.png'))
+    assert img.dtype == np.uint8, f'Wrong dtype: {img.dtype}'
+
+    with grpc.insecure_channel(
+            'localhost:50042',
+            options=[('grpc.max_send_message_length', 50000000), ('grpc.max_receive_message_length', 50000000)]
+    ) as channel:
+        stub = prediction_server_pb2_grpc.PredictionServerStub(channel)
+        response = stub.predict(prediction_server_pb2.PredictionRequest(
+            image=img.tobytes(),
+            height=img.shape[0],
+            width=img.shape[1],
+            channels=img.shape[2],
+        ))
+
+    prediction = np.frombuffer(response.image, dtype=response.dtype).reshape(*img.shape)
+
+    print(f"[Prediction client] [predict] got message from server")
+    plt.figure(figsize=(15, 15))
+    plt.imshow(prediction)
+    plt.show()
+
+
+def main():
+    predict()
+
+
+if __name__ == '__main__':
+    main()
