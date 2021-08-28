0d4132cddff885a40f0894b9b912b4d1a2263fcc
TheFebrin <dawid.dieu@nomagic.ai>
2021-08-28 19:23:42 +0200

    Function to find rotation between two vectors

--- utils/util_functions.py
+++ utils/util_functions.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import numba
 import matplotlib.pyplot as plt
 
+
 @numba.jit(nopython=True)
 def get_normals_from_depth(depth_img_arr: np.ndarray) -> np.ndarray:
     n, m = depth_img_arr.shape
@@ -18,6 +19,39 @@ def get_normals_from_depth(depth_img_arr: np.ndarray) -> np.ndarray:
             magnitude = np.sqrt((direction ** 2).sum())
             normals_img_arr[x, y] = direction / magnitude
     return normals_img_arr
+
+
+def rotation_between_normals(a, b):
+    assert len(b) == len(a) == 3
+    assert a.dtype == b.dtype == np.float32
+
+    a /= np.linalg.norm(a)
+    b /= np.linalg.norm(b)
+
+    v = np.cross(a, b)
+
+    s = np.linalg.norm(v)
+
+    c = np.dot(a, b)
+
+    vx = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+
+    #     r = np.eye(3) + vx + vx @ vx * (1 - c) / (s ** 2)
+    r = np.eye(3) + vx + vx @ vx * (1 / (1 + c))  # better formula
+    return r
+
+
+def rotation_to_quaternion(r):
+    return PyKDL.Rotation(*r.ravel()).GetQuaternion()
+
+
+def rotation_to_euler(r):
+    return PyKDL.Rotation(*r.ravel()).GetEulerZYX()
+
+
+def rotation_to_euler_zyx(r):
+    euler = PyKDL.Rotation(*r.ravel()).GetEulerZYX()
+    return tuple(map(lambda x: x * 180 / np.pi, euler))
 
 
 if __name__ == '__main__':
